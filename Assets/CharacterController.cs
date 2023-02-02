c1f6492ce43b5a87bef3fcd60632024e5b2710e1
Oliver Bozykowski <113114202+AgentOsiol@users.noreply.github.com>
2023-02-02 15:30:12 +0000

    added climbing

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -27,10 +27,8 @@ public class CharacterController : MonoBehaviour
     public GameObject groundChecker;
     public LayerMask groundLayer;
 
- 
-
-
-
+    bool climbMode = false;
+    bool inClimbTrigger = false;
 
 
     // Start is called before the first frame update
@@ -48,47 +46,66 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
-
-        if (isOnGround && Input.GetKeyDown(KeyCode.Space))
+        if ((inClimbTrigger == true) && Input.GetKey(KeyCode.W))
         {
-            myRigidbody.AddForce(transform.up * jumpForce);
+            myRigidbody.useGravity = false;
+            myRigidbody.position += (Vector3.up * Time.deltaTime);
+        } else
+        {
+            myRigidbody.useGravity = true;
+
+            isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+
+            if (isOnGround && Input.GetKeyDown(KeyCode.Space))
+            {
+                myRigidbody.AddForce(transform.up * jumpForce);
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                playerCollider.height = 2;
+                maxSpeed = sprintSpeed;
+            }
+            else if (Input.GetKey(KeyCode.LeftControl))
+            {
+                maxSpeed = crouchSpeed;
+                playerCollider.height = 1;
+            }
+            else
+            {
+                playerCollider.height = 2;
+                maxSpeed = normalSpeed;
+            }
+
+            Vector3 newVelocity = (transform.forward * Input.GetAxis("Vertical") * maxSpeed) + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
+
+            myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
+
+            rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
+            transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
+
+            camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
+            cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
+
+            camRotation = Mathf.Clamp(camRotation, -30.0f, 40.0f);
         }
-        
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            playerCollider.height = 2;
-            maxSpeed = sprintSpeed;
-        } 
-        else if (Input.GetKey(KeyCode.LeftControl))
-        {
-            maxSpeed = crouchSpeed;
-            playerCollider.height = 1;
-        } 
-        else
-        {
-            playerCollider.height = 2;
-            maxSpeed = normalSpeed;
-        }
-
-        Vector3 newVelocity = (transform.forward * Input.GetAxis("Vertical") * maxSpeed) + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
-
-        myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
-
-        rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
-        transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
-
-        camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
-        cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
-
-        camRotation = Mathf.Clamp(camRotation, -30.0f, 40.0f);
     }
 
-    private void OnTriggerEnter(Collider Player)
+    private void OnTriggerEnter(Collider other)
     {
-        if (Player.tag == "climb")
+
+        if (other.tag == "climb")
         {
-            Rigidbody.AddForce
+
+            inClimbTrigger = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.tag == "climb")
+        {
+            inClimbTrigger = false;
         }
     }
 
